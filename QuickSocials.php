b919a8d2ce0c77e4364389b66102642b610ef528
Jeroen H <jeroen-h@hotmail.nl>
2020-05-19 22:47:51 +0200

    Bugs be gone!

--- QuickSocials.php
+++ QuickSocials.php
@@ -7,7 +7,7 @@
  * Author: Jeroen Huiskes
  * Author URI: https://www.rac-software.nl
  **/
-$text = 'Test';
+
 add_action('admin_menu', 'quicksocials_plugin_setup_menu');
 
 function quicksocials_plugin_setup_menu()
@@ -19,54 +19,56 @@ function quicksocials_init()
 {
     ?>
     <div>
-        <h2>footer_word Options</h2>
+        <h2>Fill Info:</h2>
         <form method="post" action="options.php">
             <?php wp_nonce_field('update-options'); ?>
-            <table width="510">
-                <tr valign="top">
-                    <th width="92" scope="row">Enter Email</th>
-                    <td width="406">
-                        <input name="footer_word_data" type="text" id="footer_word_data" value="<?php echo get_option('footer_word_data'); ?>" />
-                    </td>
-                </tr>
-                <tr valign="top">
-                    <th width="92" scope="row">Enter Subject</th>
-                    <td width="406">
-                        <input name="email_subject" type="text" id="email_subject" value="<?php echo get_option('email_subject'); ?>" />
-                    </td>
-                </tr>
-                <tr valign="top">
-                    <th width="92" scope="row">Enter Message</th>
-                    <td width="406">
-                        <input name="email_message" type="text" id="email_message" value="<?php echo get_option('email_message'); ?>" />
-                    </td>
-                </tr>
-            </table>
+
+            <input name="email_adress" type="text" id="email_adress" value="<?php echo get_option('email_adress'); ?>" />
+
             <input type="hidden" name="action" value="update" />
-            <input type="hidden" name="page_options" value="footer_word_data" />
+            <input type="hidden" name="page_options" value="email_adress" />
             <p>
-                <input type="submit" value="<?php _e('Save Changes') ?>" />
+                <input type="submit" value="<?php _e('Save Email Addres') ?>" />
+            </p>
+        </form>
+        <form method="post" action="options.php">
+            <?php wp_nonce_field('update-options'); ?>
+
+            <input name="email_subject" type="text" id="email_subject" value="<?php echo get_option('email_subject'); ?>" />
+
+            <input type="hidden" name="action" value="update" />
+            <input type="hidden" name="page_options" value="email_subject" />
+            <p>
+                <input type="submit" value="<?php _e('Save Email Subject') ?>" />
+            </p>
+        </form>
+        <form method="post" action="options.php">
+            <?php wp_nonce_field('update-options'); ?>
+
+            <input name="email_message" type="text" id="email_message" value="<?php echo get_option('email_message'); ?>" />
+
+            <input type="hidden" name="action" value="update" />
+            <input type="hidden" name="page_options" value="email_message" />
+            <p>
+                <input type="submit" value="<?php _e('Save Email Message') ?>" />
             </p>
         </form>
     </div>
     <?php
 }
 
-function change_footer_admin() {
-    echo get_option('footer_word_data');
-}
-
-add_filter('admin_footer_text', 'change_footer_admin');
 
 
 
 function send_email_NP(){
-     $socials = get_option('footer_word_data');
-     $header = get_option('email_subject');
-     $text = get_option('email_message');
+    $socials = get_option('footer_word_data');
+    $header = get_option('email_subject');
+    $text = get_option('email_message');
     wp_mail($socials, $header, $text);
 
-   return $post_id;
+
+    return $post_id;
 }
- add_action( 'publish_post', 'send_email_NP');
+
+add_action( 'publish_post', 'send_email_NP');
 
